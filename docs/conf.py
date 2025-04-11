181b4f90d84ad957857c5863490fb41e1a20be11
Dan Calavrezo <dan.calavrezo.ext@qorix.ai>
2025-04-11 12:02:51 +0300

    test

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,12 @@ needs_id_required = True
 needs_build_json = True
 master_doc = 'index'
 
+
+# Optional but helps:
+needs_version = "v1"
+needs_versions = {"v1": "Initial version"}
+needs_current_version = "v1"
+
 # Paths: include your docs folder so Sphinx can find index.rst
 sys.path.insert(0, os.path.abspath('.'))
 
